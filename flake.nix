f5f60e547e50b196ab91cb2fda5b9e48c0ab7f20
ivnlpz <ivan@ivanlopez.info>
2024-11-16 01:03:28 +0900

    Added the flakes

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Archive.org-Downloader Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (python311.withPackages (
+              ps: with ps; [
+                tqdm
+                img2pdf
+                requests
+              ]
+            ))
+          ];
+        };
+      }
+    );
+}
